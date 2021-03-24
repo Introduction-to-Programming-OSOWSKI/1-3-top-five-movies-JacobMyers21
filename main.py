49dae79002b0b8c88e972b928f62cf01faccf6ef
JacobMyers21 <813862@mystma.org>
2021-03-24 15:22:51 +0000

    attempt 1 movies

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies ():
+   return "Lord of the Rings " + "Shrek 2 " + "Forest Gump " + "Skyrim Opening " + "Bingo Bango Bongo"
+
+print(topFiveMovies())
