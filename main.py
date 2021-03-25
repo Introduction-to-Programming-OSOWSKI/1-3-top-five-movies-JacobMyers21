c11c886a1c9459bbdf53722ce66efd6ecbb208ce
JacobMyers21 <813862@mystma.org>
2021-03-25 14:33:10 +0000

    topmovies final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
-def topFiveMovies ():
-   return "Lord of the Rings " + "Shrek 2 " + "Forest Gump " + "Skyrim Opening " + "Bingo Bango Bongo"
+def topFiveMovies (a, b, c, d, e):
+   return a + " " + b + " " + c + " " + d + " " + e
 
-print(topFiveMovies())
+print(topFiveMovies("Lord of the Rings," , "Shrek 2," ,  "Forest Gump," , "Skyrim Opening," , "Bingo Bango Bongo"))
